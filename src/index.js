ad164678b6455660657568109904175725d8c811
jessicakwalters <jessicakwalters@gmail.com>
2019-09-18 13:29:48 -0700

    part 1 complete

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles.css';
+
+
+class Message extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Hello World' };
+  }
+
+  render() {
+    return <div>{this.state.text}</div>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+       <Message />
+    );
+  }
+}
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
